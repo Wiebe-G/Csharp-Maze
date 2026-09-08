f417fc73080e1c2dde28ef5a8013b1f234b72abc
Wiebe Gouma <goumawiebe1234@proton.me>
2026-09-08 09:15:03 +0200

    Gebruiker kan bestand kiezen

--- Csharp-Doolhof/Program.cs
+++ Csharp-Doolhof/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,8 @@ namespace Csharp_Doolhof
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("o/");
+            SelectFile f = new SelectFile();
+            f.UserInputForFileSelect();
         }
     }
 }
---
+++ Csharp-Doolhof/SelectFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Doolhof
+{
+    internal class SelectFile
+    {
+        internal void UserInputForFileSelect()
+        {
+            string RootPath = AppDomain.CurrentDomain.BaseDirectory;
+            IEnumerable<string> Files = Directory.EnumerateFiles(
+                path: RootPath,
+                searchPattern: "*.txt",
+                searchOption: SearchOption.AllDirectories
+                );
+            int index = 0;
+            foreach (string File in Files)
+            {
+                Console.WriteLine($"{index}: {File}");
+                index++;
+            }
+            Console.WriteLine("Welk bestand wilt u gebruiken?");
+
+            bool IsValidInput = false;
+            bool Input;
+            while (!IsValidInput)
+            {
+                Input = int.TryParse(Console.ReadLine(), out index);
+                if (Input)
+                {
+                    Console.WriteLine($"Bestand {Files.ElementAt(index)} gekozen, laden...");
+                    IsValidInput = true;
+                    break;
+                }
+                Console.WriteLine("Input klopt niet, probeer het opnieuw.");
+            }
+            string ChosenFile = Files.ElementAt(index);
+        }
+    }
+}
