7e3612ea478df57d523be56bc59e421f303bafed
Wiebe Gouma <goumawiebe1234@proton.me>
2026-09-08 10:42:42 +0200

    Illegaal karakter stopt nu het programma

--- Csharp-Doolhof/Game.cs
+++ Csharp-Doolhof/Game.cs
@@ -20,21 +20,36 @@ namespace Csharp_Doolhof
 
         internal bool IsValidFile(string FilePath)
         {
-            using (StreamReader sr = new StreamReader(FilePath))
+            //char[,] Maze = new char[,];
+            char[] AllowedChars = ['#', '.', 'E', 'e', 'S', 's', 'K', 'k', 'D', 'd'];
+            using (StreamReader sr = new(FilePath))
             {
                 string line;
+                int Column = 0;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    int Column = 0;
                     for (int Row = 0; Row < line.Length; Row++)
                     {
                         Console.Write($"Array {Column}-{Row}: ");
+                        //Maze[Column, Row] = line[Column][Row];
                         Console.WriteLine(line[Row]);
+                        if (!AllowedChars.Contains(line[Row]))
+                        {
+                            EndLoopDueToIllegalCharAtColumnRow(Column, Row);
+                        }
+
                     }
+                    Console.WriteLine("Nieuwe rij");
                     Column++;
                 }
             }
             return true;
+        }
+
+        internal void EndLoopDueToIllegalCharAtColumnRow(int Column, int Row)
+        {
+            Console.WriteLine($"Illegaal karakter gevonden op {Column}-{Row}. Verwijder dat karakter.");
+            Environment.Exit(1);
         }
     }
 }
