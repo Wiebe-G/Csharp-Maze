549c03039a92e680ed3c48bf4b3db5d1c6793c39
Wiebe Gouma <goumawiebe1234@proton.me>
2026-09-22 11:38:30 +0200

    Win conditie checks verplaatst

--- Csharp-Doolhof/Game.cs
+++ Csharp-Doolhof/Game.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Linq;
 using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Csharp_Doolhof
 {
@@ -103,6 +105,27 @@ namespace Csharp_Doolhof
                         continue;
                     }
 
+                    if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('K'))
+                    {
+                        Maze[Pos.PlayerX, Pos.PlayerY] = '.';
+                        Pos.HasKey = true;
+                    }
+
+                    if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('D') && !Pos.HasKey)
+                    {
+                        return;
+                    }
+
+                    if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('D') && Pos.HasKey)
+                    {
+                        Maze[Pos.PlayerX, Pos.PlayerY] = '.';
+                    }
+
+                    if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('E'))
+                    {
+                        WinScreen();
+                    }
+
                     Console.Clear();
                     RenderMaze(Maze);
                 }
@@ -123,6 +146,31 @@ namespace Csharp_Doolhof
             catch (IndexOutOfRangeException)
             {
                 return false;
+            }
+        }
+
+        private void WinScreen()
+        {
+            Console.WriteLine("YOU WIN!!!!! YIPPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!");
+            Console.WriteLine("Wilt u opnieuw spelen? [Y]es/[N]o");
+            string Choice = Console.ReadLine();
+            switch (Choice)
+            {
+                case string when Choice.StartsWith("Y"):
+                    Console.WriteLine("Ok");
+                    SelectAndValidateFile s = new();
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    s.UserInputForFileSelect();
+                    break;
+                case string when Choice.StartsWith("N"):
+                    Console.WriteLine("Ok");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Ongeldige keuze. Dat is dan waarschijnlijk een nee");
+                    Environment.Exit(0);
+                    break;
             }
         }
     }
--- Csharp-Doolhof/Movement.cs
+++ Csharp-Doolhof/Movement.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace Csharp_Doolhof
 {
@@ -40,45 +39,6 @@ namespace Csharp_Doolhof
                 return false;
             }
 
-            if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('K'))
-            {
-                Maze[Pos.PlayerX, Pos.PlayerY] = '.';
-                Pos.HasKey = true;
-            }
-
-            if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('D') && !Pos.HasKey)
-            {
-                return false;
-            }
-
-            if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('D') && Pos.HasKey)
-            {
-                Maze[Pos.PlayerX, Pos.PlayerY] = '.';
-            }
-
-            if (Maze[Pos.PlayerX, Pos.PlayerY].Equals('E'))
-            {
-                Console.WriteLine("YOU WIN!!!!! YIPPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!");
-                Console.WriteLine("Wilt u opnieuw spelen? [Y]es/[N]o");
-                string Choice = Console.ReadLine();
-                switch (Choice)
-                {
-                    case string when Choice.StartsWith("Y"):
-                        Console.WriteLine("Ok");
-                        SelectAndValidateFile s = new();
-                        Thread.Sleep(2000);
-                        s.UserInputForFileSelect();
-                        break;
-                    case string when Choice.StartsWith("N"):
-                        Console.WriteLine("Ok");
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Ongeldige keuze. Dat is dan waarschijnlijk een nee");
-                        Environment.Exit(0);
-                        break;
-                }
-            }
             return true;
         }
 
@@ -86,7 +46,7 @@ namespace Csharp_Doolhof
         {
             if (
                 Maze[Pos.PlayerX, Pos.PlayerY].Equals('#') ||
-                Maze[Pos.PlayerX, Pos.PlayerY].Equals('D') && !Pos.HasKey ||
+                (Maze[Pos.PlayerX, Pos.PlayerY].Equals('D') && !Pos.HasKey) ||
                 Pos.PlayerY < 0 ||
                 Pos.PlayerX < 0 ||
                 Pos.PlayerY > Maze.GetLength(1) ||
